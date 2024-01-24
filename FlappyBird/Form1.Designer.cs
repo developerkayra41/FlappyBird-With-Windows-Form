a061774c75ea21265dc49470d8731d432684c8cb
kayra <kayra@DESKTOP-ANF5MEQ>
2024-01-24 14:01:10 +0300

    Add project files.

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pipeBot = new System.Windows.Forms.PictureBox();
+            this.zemin = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(216)))), ((int)(((byte)(149)))));
+            this.panel1.Location = new System.Drawing.Point(-5, 530);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(993, 160);
+            this.panel1.TabIndex = 4;
+            // 
+            // pipeBot
+            // 
+            this.pipeBot.Image = global::FlappyBird.Properties.Resources.pipePNG;
+            this.pipeBot.Location = new System.Drawing.Point(533, 262);
+            this.pipeBot.Name = "pipeBot";
+            this.pipeBot.Size = new System.Drawing.Size(70, 279);
+            this.pipeBot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBot.TabIndex = 5;
+            this.pipeBot.TabStop = false;
+            // 
+            // zemin
+            // 
+            this.zemin.Image = global::FlappyBird.Properties.Resources.basePNG;
+            this.zemin.Location = new System.Drawing.Point(-15, 481);
+            this.zemin.Name = "zemin";
+            this.zemin.Size = new System.Drawing.Size(337, 111);
+            this.zemin.TabIndex = 1;
+            this.zemin.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.Image = global::FlappyBird.Properties.Resources.birdPNG;
+            this.bird.Location = new System.Drawing.Point(144, 161);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(50, 37);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Playpen Sans", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 48);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Score : 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FlappyBird.Properties.Resources.basePNG;
+            this.pictureBox1.Location = new System.Drawing.Point(319, 481);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 111);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::FlappyBird.Properties.Resources.basePNG;
+            this.pictureBox3.Location = new System.Drawing.Point(651, 481);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(337, 111);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::FlappyBird.Properties.Resources.pipeTopPNG;
+            this.pipeTop.Location = new System.Drawing.Point(505, -209);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(70, 279);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 6;
+            this.pipeTop.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("SimSun", 100.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(448, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 167);
+            this.label3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(114, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kolay";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(114, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Zor";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.ForeColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(319, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(328, 211);
+            this.panel2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(80, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 35);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Zorluk Seç";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.zemin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pipeBot);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Bird";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox zemin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBot;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+
+        #region DEĞİŞKENLER
+        int gravity = 10;
+        int pipeSpeed = 10;
+        int score = 0;
+        int firstScore = 0;
+        int lastScore = 0;
+        int randomY;
+        short distance;
+        sbyte speedRate;
+
+        Random rnd = new Random();
+        #endregion
+
+        #region YAPICI VE LOAD METODU
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region TİMER
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bird.Top += gravity;
+            if (bird.Top <= 2) bird.Top = 3;
+
+            pipeTop.Left -= pipeSpeed;
+            pipeBot.Left -= pipeSpeed;
+
+            if (pipeBot.Left < -85)
+            {
+                pipeTop.Left = 974;
+                pipeBot.Left = 1000;
+                randomY = rnd.Next(262, 381);
+                pipeBot.Top = randomY;
+                pipeTop.Top = pipeBot.Top - distance;
+                score++; label1.Text = string.Format($"Score: {score}");
+            }
+
+            if (score % speedRate == 0 || score != 0) firstScore = score;
+
+            if (score >= lastScore + speedRate && score < firstScore + speedRate)
+            {
+                lastScore += speedRate;
+                pipeSpeed += 2;
+            }
+
+            if (bird.Bounds.IntersectsWith(pipeBot.Bounds) || bird.Bounds.IntersectsWith(pipeTop.Bounds)
+                || bird.Bounds.IntersectsWith(zemin.Bounds))
+            {
+                GameOver();
+                MessageBox.Show($"Oyun Bitti ! Skorunuz {score}", "Kaybettin !");
+            }
+        }
+        #endregion
+
+        #region OYUN BİTTİ METODU
+        void GameOver()
+        {
+            timer1.Stop();
+        }
+        #endregion
+
+        #region TUŞ KONTROLU 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) gravity = -10;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) gravity = 10;
+        }
+        #endregion
+
+        #region OYUN ZORLUĞU SEÇİMİ
+        private void button1_Click(object sender, EventArgs e)
+        {
+            distance = 471; speedRate = 5; panel2.Dispose(); timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            distance = 430; speedRate = 3; panel2.Dispose(); timer1.Enabled = true;
+        }
+        #endregion
+    }
+}
